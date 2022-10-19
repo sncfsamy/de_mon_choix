2e4957c4e7362abcdda28db8610249fd29c27262
sncfsamy <samy.lamiri@sncf.fr>
2022-10-19 11:28:04 +0200

    init and publish

---
+++ index.js
@@ -0,0 +1,8 @@
+const wilder = require('./information');
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm " + wilder.name + " from " + wilder.campus +" campus",
+    e : "øø",
+    T : "VV "
+}));
---
+++ information.js
@@ -0,0 +1,6 @@
+const wilder = {
+    name:"Samy",
+    campus:"Nantes"
+}
+
+module.exports = wilder;
