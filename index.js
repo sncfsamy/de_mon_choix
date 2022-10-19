99a0c8b372ce800c193ff3a90452f553cf24798e
sncfsamy <samy.lamiri@sncf.fr>
2022-10-19 12:31:16 +0200

    animal change

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const wilder = require('./information');
-const cowsay = require("cowsay");
+const wilder = require('./information')
+const cowsay = require('cowsay')
 
 console.log(cowsay.say({
     text : "I'm " + wilder.name + " from " + wilder.campus +" campus",
     e : "øø",
-    T : "VV "
+    T : "VV ",
+    f: "dalek"
 }));
