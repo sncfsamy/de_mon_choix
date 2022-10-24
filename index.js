e7b7b04896c74702e41085e7bbff4d9525a09d95
sncfsamy <samy.lamiri@sncf.fr>
2022-10-24 16:03:08 +0200

    =P

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const wilder = require('./information')
 const cowsay = require('cowsay')
 
 console.log(cowsay.say({
-    text : "I'm " + wilder.name + " from " + wilder.campus +" campus",
+    text : "I'm " + wilder.name + " from " + wilder.campus +" campus. EXTERMINATE !",
     e : "øø",
     T : "VV ",
-    f: "dalek"
+    f: "dalek-shooting"
 }));
